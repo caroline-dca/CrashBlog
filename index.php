a000d9c50fc0ef3abcd96a661c41fa4151897caa
LaureBre <acs.laure.eole@gmail.com>
2018-02-26 15:39:59 +0100

    Add index with 1st css

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Crash Blog</title>
+  <link rel="stylesheet" href="css/base.css">
+  <link href="https://fonts.googleapis.com/css?family=Krona+One|Montserrat|Titillium+Web" rel="stylesheet">
+</head>
+<body>
+
+  <header>
+    <h1>Crash Blog</h1>
+  </header>
+
+  <main>
+
+    <p>Bienvenue sur le blog des Crash Coders. Vous trouverez ici nos articles, publiés au fil de la formation.</p>
+    <p>Sentez-vous libres de contribuer !</p>
+
+    <section>
+
+        <div id='creation'>
+          <h2>Création :</h2>
+          <ul>
+            <li><a href="auteur.php">Auteur</a></li>
+            <li><a href="categorie.php">Catégorie</a></li>
+            <li><a href="article.php">Article</a></li>
+          </ul>
+        </id>
+      </div>
+
+      <div class="separateur"></div>
+
+      <div id='consult'>
+        <h2>Consultation :</h2>
+        <ul>
+          <li><a href="consultation.php">Articles en ligne</a></li>
+        </ul>
+      </div>
+
+    </section>
+  </main>
+</body>
+</html>
