796ce5f282e4935fa737fc19848d5cd627aab3e3
gautier.keitaro <keitarohd@gmail.com>
2018-02-28 12:14:09 +0100

    article final

--- article.php
+++ article.php
@@ -20,7 +20,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Crash Blog</title>
-  <link rel="stylesheet" href="css/article.css">
   <link href="https://fonts.googleapis.com/css?family=Krona+One|Montserrat|Titillium+Web" rel="stylesheet">
 </head>
 <body>
